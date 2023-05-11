4278f6e60a1177b41f5805586d2ec0780457c046
Rajayne <ryann.j.nealey@gmail.com>
2023-05-11 15:44:50 -0700

    Create useState for NewBoxForm Set initialState to form object with empty string values

--- src/ColorBox/NewBoxForm.js
+++ src/ColorBox/NewBoxForm.js
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewBoxForm.css";
 
 const NewBoxForm = () => {
+  const initialState = { color: "", height: "", width: "" };
+  const [formData, setFormData] = useState(initialState);
+  const { color, height, width } = formData;
   return (
     <div className="NewBoxForm">
       <h2>New Box Form</h2>
       <form className="NewBoxForm-form">
         <div className="NewBoxForm-color">
           <label className="NewBoxForm-label" htmlFor="color">
-            Box Color:{" "}
+            Box Color:
           </label>
           <input
             className="NewBoxForm-input"
             id="color"
             name="color"
             type="color"
+            value={color}
           ></input>
         </div>
         <div className="NewBoxForm-height">
           <label className="NewBoxForm-label" htmlFor="height">
-            Box Height(px):{" "}
+            Box Height(px):
           </label>
           <input
             className="NewBoxForm-input"
             id="height"
             name="height"
             type="number"
+            value={height}
           ></input>
         </div>
         <div className="NewBoxForm-width">
           <label className="NewBoxForm-label" htmlFor="width">
-            Box Width(px):{" "}
+            Box Width(px):
           </label>
           <input
             className="NewBoxForm-input"
             id="width"
             name="width"
             type="number"
+            value={width}
           ></input>
         </div>
       </form>
