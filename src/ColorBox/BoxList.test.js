fefcdcb0a6c3adfa19042cb584e66c670536a503
Rajayne <ryann.j.nealey@gmail.com>
2023-05-12 11:16:11 -0700

    Add snapshot test to BoxList

--- src/ColorBox/BoxList.test.js
+++ src/ColorBox/BoxList.test.js
@@ -4,3 +4,8 @@ import BoxList from "./BoxList";
 test("renders BoxList without crashing", () => {
   render(<BoxList />);
 });
+
+test("matches snapshot", () => {
+  const { asFragment } = render(<BoxList />);
+  expect(asFragment()).toMatchSnapshot();
+});
