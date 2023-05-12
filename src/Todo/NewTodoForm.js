004c7411999557ccd80fa5564116c1b542714d8c
Rajayne <ryann.j.nealey@gmail.com>
2023-05-12 12:25:59 -0700

    Create state to track formData Create handleChange and handleSubmit functions

--- src/Todo/NewTodoForm.js
+++ src/Todo/NewTodoForm.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewTodoForm = ({ addTodo }) => {
-  const handleSubmit = () => {
-    alert("Submitted!");
+  const initialState = { key: "", text: "" };
+  const [formData, setFormData] = useState(initialState);
+  const handleChange = (e) => {
+    setFormData((formData) => ({ ...formData, text: e.target.value }));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Submitted ${formData.text}!`);
+    setFormData(initialState);
+  };
+
   return (
     <div>
       <h2>New Todo Form</h2>
       <form className="newTodoForm" onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
           name="newTodo-input"
           id="newTodo-input"
           type="text"
           placeholder="New Todo"
+          value={formData.text}
         ></input>
         <button>Submit!</button>
       </form>
