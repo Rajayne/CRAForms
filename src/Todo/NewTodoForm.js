fcb11eda58703f1d828028b2ce3837d28ec17d73
Rajayne <ryann.j.nealey@gmail.com>
2023-05-12 11:56:17 -0700

    Create NewTodoForm component

--- src/Todo/NewTodoForm.js
+++ src/Todo/NewTodoForm.js
@@ -4,6 +4,15 @@ const NewTodoForm = () => {
   return (
     <div>
       <h2>New Todo Form</h2>
+      <form className="newTodoForm">
+        <input
+          name="newTodo-input"
+          id="newTodo-input"
+          type="text"
+          placeholder="New Todo"
+        ></input>
+        <button>Submit!</button>
+      </form>
     </div>
   );
 };
